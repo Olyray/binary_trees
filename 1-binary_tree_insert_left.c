e5409784965be752c58548533e6b8ca8c3c73bb1
Olamide <olire96@gmail.com>
2023-03-01 21:45:11 +0100

    the second task

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - Creates the node for a binary tree
+ * binary_tree_insert_left - Insert a node to the left
  * @parent: the parent of the binary tree
  * @value: the value for the binary tree
  * Return: The created binary tree
@@ -10,6 +10,7 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *left_node;
+	binary_tree_t *node_pointer;
 
 	if (parent == NULL)
 		return (NULL);
@@ -17,12 +18,17 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (left_node == NULL)
 		return (NULL);
 	left_node->n = value;
+	left_node->left = NULL;
+	left_node->right = NULL;
 	left_node->parent = parent;
 	if (parent->left == NULL)
 		parent->left = left_node;
 	else
-		parent->left->parent = left_node;
-	left_node->left = NULL;
-	left_node->right = NULL;
+	{
+		node_pointer = parent->left;
+		parent->left = left_node;
+		node_pointer->parent = left_node;
+		left_node->left = node_pointer;
+	}
 	return (left_node);
 }
